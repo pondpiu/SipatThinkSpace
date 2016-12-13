612f94c6999b6eab7f1b76dd195ada0d0a24e403
Bliss <blysxaye@icloud.com>
2016-12-14 01:45:42 +0700

    pierre-hilt

--- _rebuild/thinkingSpace/build/dev-server.js
+++ _rebuild/thinkingSpace/build/dev-server.js
@@ -23,22 +23,22 @@ var app = express();
 
 var compiler = webpack(webpackConfig);
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: false
-  }
-})
-
-var hotMiddleware = require('webpack-hot-middleware')(compiler);
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' });
-    cb()
-  })
-})
+// var devMiddleware = require('webpack-dev-middleware')(compiler, {
+//   publicPath: webpackConfig.output.publicPath,
+//   stats: {
+//     colors: true,
+//     chunks: false
+//   }
+// })
+//
+// var hotMiddleware = require('webpack-hot-middleware')(compiler);
+// // force page reload when html-webpack-plugin template changes
+// compiler.plugin('compilation', function (compilation) {
+//   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+//     hotMiddleware.publish({ action: 'reload' });
+//     cb()
+//   })
+// })
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
@@ -53,14 +53,22 @@ Object.keys(proxyTable).forEach(function (context) {
 // app.use(require('connect-history-api-fallback')())
 
 // serve webpack bundle output
-app.use(devMiddleware);
+// app.use(devMiddleware);
 
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended:true}));
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware);
+// app.use(hotMiddleware);
 
+
+// var compiler = webpack(config);
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+app.use(webpackDevMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//BodyParser
+// app.use(bodyParser.json);
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
